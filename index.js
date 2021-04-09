80fff2c56edfa0804ff8d03445e0e4b4ff224e6d
Isaiah Davis <ID@isaiahs-imac.myfiosgateway.com>
2021-04-09 11:40:55 -0400

    commit tree 70f105cf9777270ccf0402f54be613ab680defc0

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(drivers,arr){
+    return drivers.filter(match =>
+        match.toLowerCase() === arr.toLowerCase()
+        )
+    }
+function fuzzyMatch(drivers,arr2){
+    return drivers.filter(match =>
+        match.toLowerCase().indexOf(arr2.toLowerCase()) === 0
+    )
+}
+
+function matchName(drivers,arr3){
+    return drivers.filter( record => record.name === arr3)
+}
